813eb9b32aacbd864087ce6b21390ae15a7807ef
Sushant Sharma <ssushants786@gmail.com>
2024-07-26 21:14:41 +0530

    included hero.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Hero from './sections/HeroSection/Hero'
 import Navbar from './sections/Navbar/Navbar'
 
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   )
 }
