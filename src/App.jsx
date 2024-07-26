507420f0023337bd61d1e78d8ec21480020e9a14
Sushant Sharma <ssushants786@gmail.com>
2024-07-26 22:29:03 +0530

    included footer.jsx in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Footer from './sections/Footer/Footer'
 import Hero from './sections/HeroSection/Hero'
 import Navbar from './sections/Navbar/Navbar'
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <Navbar />
       <Hero />
+      <Footer />
     </>
   )
 }
