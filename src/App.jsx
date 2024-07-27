ce48b5820b1b301979682eface4b469d64811277
Sushant Sharma <ssushants786@gmail.com>
2024-07-27 12:44:22 +0530

    app.jsx completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import Footer from "./sections/Footer/Footer";
 import Hero from "./sections/HeroSection/Hero";
 import Navbar from "./sections/Navbar/Navbar";
-
 function App() {
   const companyLinks = [
     { href: "#", text: "About Us" },
